08d9814b014deccca6503f118c51c4fc343d5b0b
rocky <rocky>
2006-02-10 03:55:19 +0000

    Work on device capabilities

--- examples/device.pl
+++ examples/device.pl
@@ -39,36 +39,37 @@ my $vcid ='$Id$';
 # Prints out drive capabilities
 sub print_drive_capabilities($$$) {
     my ($i_read_cap, $i_write_cap, $i_misc_cap) = @_;
-  if ($perlcdio::DRIVE_CAP_ERROR == $i_misc_cap) {
+  if ($i_misc_cap->{DRIVE_CAP_ERROR}) {
     printf("Error in getting drive hardware properties\n");
   } else {
     printf("Hardware                    : %s\n", 
-	   $i_misc_cap & $perlcdio::DRIVE_CAP_MISC_FILE  
+	   $i_misc_cap->{DRIVE_CAP_MISC_FILE}  
 	   ? "Disk Image"  : "CD-ROM or DVD");
     printf("Can eject                   : %s\n", 
-	   $i_misc_cap & $perlcdio::DRIVE_CAP_MISC_EJECT
+	   $i_misc_cap->{DRIVE_CAP_MISC_EJECT}
 	   ? "Yes" : "No");
     printf("Can close tray              : %s\n", 
-	   $i_misc_cap & $perlcdio::DRIVE_CAP_MISC_CLOSE_TRAY
+	   $i_misc_cap->{DRIVE_CAP_MISC_CLOSE_TRAY}
 	   ? "Yes" : "No");
     printf("Can disable manual eject    : %s\n", 
-	   $i_misc_cap & $perlcdio::DRIVE_CAP_MISC_LOCK          
+	   $i_misc_cap->{DRIVE_CAP_MISC_LOCK}          
 	   ? "Yes" : "No");
     printf("Can select juke-box disc    : %s\n\n", 
-	   $i_misc_cap & $perlcdio::DRIVE_CAP_MISC_SELECT_DISC   
+	   $i_misc_cap->{DRIVE_CAP_MISC_SELECT_DISC}   
 	   ? "Yes" : "No");
 
     printf("Can set drive speed         : %s\n", 
-	   $i_misc_cap & $perlcdio::DRIVE_CAP_MISC_SELECT_SPEED  
+	   $i_misc_cap->{DRIVE_CAP_MISC_SELECT_SPEED}  
 	   ? "Yes" : "No");
-    printf("Can detect if CD changed    : %s\n", 
-	   $i_misc_cap & $perlcdio::DRIVE_CAP_MISC_MEDIA_CHANGED 
-	   ? "Yes" : "No");
+# Don't think this bit is set accurately. 
+#    printf("Can detect if CD changed    : %s\n", 
+#	   $i_misc_cap->{DRIVE_CAP_MISC_MEDIA_CHANGED} 
+#	   ? "Yes" : "No");
     printf("Can read multiple sessions  : %s\n", 
-	   $i_misc_cap & $perlcdio::DRIVE_CAP_MISC_MULTI_SESSION 
+	   $i_misc_cap->{DRIVE_CAP_MISC_MULTI_SESSION} 
 	   ? "Yes" : "No");
     printf("Can hard reset device       : %s\n\n", 
-	   $i_misc_cap & $perlcdio::DRIVE_CAP_MISC_RESET         
+	   $i_misc_cap->{DRIVE_CAP_MISC_RESET}         
 	   ? "Yes" : "No");
   }
   
@@ -78,16 +79,52 @@ sub print_drive_capabilities($$$) {
   } else {
     printf("Reading....\n");
     printf("  Can play audio            : %s\n", 
-	   $i_read_cap & $perlcdio::DRIVE_CAP_READ_AUDIO      
+	   $i_read_cap->{DRIVE_CAP_READ_AUDIO}      
+	   ? "Yes" : "No");
+    printf("  Can read  CD-DA           : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_CD_DA}       
+	   ? "Yes" : "No");
+    printf("  Can read  CD+G            : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_CD_G}       
 	   ? "Yes" : "No");
     printf("  Can read  CD-R            : %s\n", 
-	   $i_read_cap & $perlcdio::DRIVE_CAP_READ_CD_R       
+	   $i_read_cap->{DRIVE_CAP_READ_CD_R}       
 	   ? "Yes" : "No");
     printf("  Can read  CD-RW           : %s\n", 
-	   $i_read_cap & $perlcdio::DRIVE_CAP_READ_CD_RW      
+	   $i_read_cap->{DRIVE_CAP_READ_CD_RW}      
+	   ? "Yes" : "No");
+    printf("  Can read  DVD-R           : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_DVD_R}    
+	   ? "Yes" : "No");
+    printf("  Can read  DVD+R           : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_DVD_PR}    
+	   ? "Yes" : "No");
+    printf("  Can read  DVD-RAM         : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_DVD_RAM}    
 	   ? "Yes" : "No");
     printf("  Can read  DVD-ROM         : %s\n", 
-	   $i_read_cap & $perlcdio::DRIVE_CAP_READ_DVD_ROM    
+	   $i_read_cap->{DRIVE_CAP_READ_DVD_RW}    
+	   ? "Yes" : "No");
+    printf("  Can read  DVD-ROM         : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_DVD_RPW}    
+	   ? "Yes" : "No");
+    printf("  Can read  DVD+RW          : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_DVD_ROM}    
+	   ? "Yes" : "No");
+    printf("  Can read C2 Errors        : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_C2_ERRS}    
+	   ? "Yes" : "No");
+    printf("  Can read MODE 2 FORM 1    : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_MODE2_FORM1}    
+	   ? "Yes" : "No");
+    printf("  Can read MODE 2 FORM 2    : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_MODE2_FORM2}    
+	   ? "Yes" : "No");
+    printf("  Can read MCN              : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_MCN}    
+	   ? "Yes" : "No");
+    printf("  Can read ISRC             : %s\n", 
+	   $i_read_cap->{DRIVE_CAP_READ_ISRC}    
 	   ? "Yes" : "No");
   }
   
@@ -97,11 +134,19 @@ sub print_drive_capabilities($$$) {
   } else {
     printf("\nWriting....\n");
     printf("  Can write CD-RW           : %s\n", 
-	   $i_read_cap & $perlcdio::DRIVE_CAP_READ_CD_RW     ? "Yes" : "No");
+	   $i_read_cap->{DRIVE_CAP_WRITE_CD_RW}     ? "Yes" : "No");
     printf("  Can write DVD-R           : %s\n", 
-	   $i_write_cap & $perlcdio::DRIVE_CAP_READ_DVD_R    ? "Yes" : "No");
+	   $i_write_cap->{DRIVE_CAP_WRITE_DVD_R}    ? "Yes" : "No");
     printf("  Can write DVD-RAM         : %s\n", 
-	   $i_write_cap & $perlcdio::DRIVE_CAP_READ_DVD_RAM  ? "Yes" : "No");
+	   $i_write_cap->{DRIVE_CAP_WRITE_DVD_RAM}  ? "Yes" : "No");
+    printf("  Can write DVD-RW          : %s\n", 
+	   $i_write_cap->{DRIVE_CAP_WRITE_DVD_RW}   ? "Yes" : "No");
+    printf("  Can write DVD-R+W         : %s\n", 
+	   $i_write_cap->{DRIVE_CAP_WRITE_DVD_RPW}  ? "Yes" : "No");
+    printf("  Can write Mt Rainier      : %s\n", 
+	   $i_write_cap->{DRIVE_CAP_WRITE_MT_RAINIER}? "Yes" : "No");
+    printf("  Can write Burn Proof      : %s\n", 
+	   $i_write_cap->{DRIVE_CAP_WRITE_BURN_PROOF}? "Yes" : "No");
   }
 }
 
@@ -127,9 +172,6 @@ my ($vendor, $model, $release, $drc) = $d->get_hwinfo();
 
 print "drive: $drive_name, vendor: $vendor, " .
     "model: $model, release: $release\n";
-
-printf "***Remaining Currently broken pending investigation...\n";
-exit 0;
 
 my ($i_read_cap, $i_write_cap, $i_misc_cap) =  $d->get_drive_cap();
 print_drive_capabilities($i_read_cap, $i_write_cap, $i_misc_cap);
